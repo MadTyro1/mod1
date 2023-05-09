3df0c076973efbdcbbb023e002d7329a17249f80
MadTyro1 <120458962+MadTyro1@users.noreply.github.com>
2023-05-08 21:59:55 -0500

    Add files via upload

---
+++ CardHolder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BepInEx;
+using System;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib.Cards;
+
+public class CardHolder : MonoBehaviour
+{
+    public List<GameObject> Cards;
+    public List<GameObject> HiddenCards;
+
+    internal void RegisterCards()
+    {
+        foreach (var Card in Cards)
+        {
+            CustomCard.RegisterUnityCard(Card, mod1.modInitials, Card.GetComponent<CardInfo>().cardName, true, null);
+        }
+        foreach (var Card in HiddenCards)
+        {
+            CustomCard.RegisterUnityCard(Card, mod1.modInitials, Card.GetComponent<CardInfo>().cardName, false, null);
+            ModdingUtils.Utils.Cards.instance.AddHiddenCard(Card.GetComponent<CardInfo>());
+        }
+    }
+}
---
+++ testing.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib.Cards;
+
+
+[BepInDependency("com.willis.rounds.unbound")]
+[BepInDependency("pykess.rounds.plugins.moddingutils")]
+[BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch")]
+[BepInPlugin("TC1", "mod1", "0.0.0")] 
+[BepInProcess("Rounds.exe")]
+public class mod1 : BaseUnityPlugin
+{
+    internal static string modInitials = "TC1";
+    internal static AssetBundle assets;
+
+    void Awake()
+    {
+        assets = Jotunn.Utils.AssetUtils.LoadAssetBundleFromResources("assets", typeof(mod1).Assembly);
+
+        assets.LoadAsset<GameObject>("ModCards").GetComponent<CardHolder>().RegisterCards();
+    } 
+    void Start()
+    {
+
+    }
+}
+    
+
